69cd078373019a4171151ccb6bf308376e4539fb
ji-bop <g.progleiva@gmail.com>
2022-11-24 20:41:54 -0800

    Sum of Subarray Minimums

--- C++/numSquares.cpp
+++ C++/numSquares.cpp
@@ -1,22 +1,28 @@
-class Solution {
-private: 
-        int is_square(int n) {
-            int sqrt_n = (int)(sqrt(n));
-            return (sqrt_n*sqrt_n == n);
-        }
+class Solution
+{
+private:
+    int is_square(int n)
+    {
+        int sqrt_n = (int)(sqrt(n));
+        return (sqrt_n * sqrt_n == n);
+    }
+
 public:
     // Based on Lagrange's Four Square theorem, there
     // are only 4 possible results: 1, 2, 3, 4.
-    int numSquares(int n) {
+    int numSquares(int n)
+    {
         // If n is a perfect square, return 1.
-        if (is_square(n)) {
+        if (is_square(n))
+        {
             return 1;
         }
 
         // The result is 4 if and only if n can be written in the
         // form of 4^k*(8*m+7). Please refer to
         // Legendre's Three-square theorem.
-        while ((n & 3) ==0) {
+        while ((n & 3) == 0)
+        {
             n >>= 2;
         }
         if ((n & 7) == 7) // n%8 == 7
@@ -26,13 +32,14 @@ public:
 
         // Check whether 2 is the result.
         int sqrt_n = (int)(sqrt(n));
-        for (int i = 1; i <= sqrt_n; i++) {
-            if (is_square(n - i*i)) {
+        for (int i = 1; i <= sqrt_n; i++)
+        {
+            if (is_square(n - i * i))
+            {
                 return 2;
             }
         }
 
         return 3;
-        }
-    };
-
+    }
+};
